87169f5e3271bdcc7122bcb76afa35163427d01e
choiseona <tjsdk617@naver.com>
2023-10-24 05:56:53 +0900

    [chore] css 초기화

--- src/components/Homepage/BackgroundVideo/index.tsx
+++ src/components/Homepage/BackgroundVideo/index.tsx
@@ -1,23 +1,34 @@
 import styled from "styled-components";
 
+const Wrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: 0;
+  overflow: hidden;
+`;
+
 const Video = styled.video`
   width: 100vw;
   height: 100vh;
   object-fit: cover;
-  overflow: hidden;
 `;
 
 const Source = styled.source``;
 
 function BackgroundVideo() {
   return (
-    <Video autoPlay muted loop>
-      <Source
-        data-testid="background-video"
-        src="/src/assets/video/background-video.mp4"
-        type="video/mp4"
-      />
-    </Video>
+    <Wrapper>
+      <Video autoPlay muted loop>
+        <Source
+          data-testid="background-video"
+          src="/src/assets/video/background-video.mp4"
+          type="video/mp4"
+        />
+      </Video>
+    </Wrapper>
   );
 }
 
