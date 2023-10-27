8ef3d0d397aafb54f0d3c5999b8500beb44bd867
choiseona <tjsdk617@naver.com>
2023-10-28 02:11:29 +0900

    [feat] TestSample 컴포넌트 생성

---
+++ src/components/TestProgresspage/TestSample/index.tsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+function TestSample() {
+  return (
+    <FlexColumn>
+      <Comment>사진 샘플에 대한 멘트 한 줄</Comment>
+      <Image src="/src/assets/image/puppy1.jpg" alt="강아지사진" />
+    </FlexColumn>
+  );
+}
+
+export default TestSample;
+
+const FlexColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 500px;
+`;
+const Comment = styled.div`
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Image = styled.img``;
--- src/pages/TestProgressPage/index.tsx
+++ src/pages/TestProgressPage/index.tsx
@@ -1,5 +1,14 @@
+import Background from "@/components/Common/Background";
+import Header from "@/components/Common/Header";
+import TestSample from "@/components/TestProgresspage/TestSample";
+
 function TestProgressPage() {
-  return <div>TestProgresspage</div>;
+  return (
+    <Background>
+      <Header />
+      <TestSample />
+    </Background>
+  );
 }
 
 export default TestProgressPage;
