56e904b50ecb1018fbf6e7891ccd8b501bcbca40
choiseona <tjsdk617@naver.com>
2023-10-28 00:31:25 +0900

    [feat] 배경 컴포넌트 children 적용

--- src/components/Common/Background/index.tsx
+++ src/components/Common/Background/index.tsx
@@ -1,6 +1,11 @@
 import styled from "styled-components";
-function Background() {
-  return <BackgroundColor />;
+import { ReactNode } from "react";
+
+interface props {
+  children: ReactNode;
+}
+function Background({ children }: props) {
+  return <BackgroundColor>{children}</BackgroundColor>;
 }
 
 export default Background;
