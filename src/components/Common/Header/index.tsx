dc137ec6b88a44c95d158589e1914dbd326aa915
choiseona <tjsdk617@naver.com>
2023-10-27 23:01:15 +0900

    [feat] header 컴포넌트 생성

---
+++ src/components/Common/Header/index.tsx
@@ -0,0 +1,39 @@
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+function Header() {
+  const navigate = useNavigate();
+  const handleClickLogo = () => {
+    navigate("/");
+  };
+  return (
+    <HeaderBar>
+      <Logo onClick={handleClickLogo}>AEFEA</Logo>
+    </HeaderBar>
+  );
+}
+
+export default Header;
+
+const HeaderBar = styled.header`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 70px;
+  display: flex;
+  align-items: center;
+  background-color: transparent;
+  color: #cecccc;
+  z-index: 20;
+`;
+
+const Logo = styled.div`
+  padding: 20px 30px;
+  font-size: larger;
+  font-weight: 600;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
