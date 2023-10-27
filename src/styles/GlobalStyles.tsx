55a72fa6fccba602d4c9a6fa99d59ad18e8aa2e9
choiseona <tjsdk617@naver.com>
2023-10-27 23:08:23 +0900

    [feat] css 초기화

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -2,7 +2,15 @@ import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 
 const GlobalStyles = createGlobalStyle`
-${reset}
+${reset},
+html,body {
+  width:100%;
+  height:100%;
+}
+#root {
+  width: 100%;
+  height: 100%;
+}
 `;
 
 export default GlobalStyles;
