6ef5fe2df48f61b4feecf620ae1055ee18dabd03
choiseona <tjsdk617@naver.com>
2023-11-05 17:44:35 +0900

    [feat] api 테스트

--- src/components/TestPreparepage/TestStartButton/index.tsx
+++ src/components/TestPreparepage/TestStartButton/index.tsx
@@ -1,12 +1,41 @@
-import NavigationButton from "@/components/Common/NavigationButton";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 function TestStartButton() {
-  return (
-    <NavigationButton
-      address="/test/progress"
-      content="시작하기"
-    ></NavigationButton>
-  );
+  const navigate = useNavigate();
+  const handleClickButton = () => {
+    navigate("/test/progress");
+    axios
+      .get("/api/test/start")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((Error) => {
+        console.log(Error);
+      });
+  };
+  return <Button onClick={handleClickButton}>시작하기</Button>;
 }
 
 export default TestStartButton;
+
+const Button = styled.button`
+  z-index: 20;
+  width: 150px;
+  font-size: 1.2rem;
+  font-weight: 550;
+  padding: 6px 0;
+  border-radius: 10px;
+  color: #cecccc;
+  border: none;
+  background-color: rgba(255, 255, 255, 0.25);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &:hover {
+    cursor: pointer;
+    background-color: rgba(255, 255, 255, 0.3);
+  }
+`;
